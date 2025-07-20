669fd3df2018094fd99d7b49f87d3c16fd56caeb
Abdallah mostafa <3bdallahgoat@gmail.com>
2025-07-20 18:16:06 +0300

    Add function-based and class-based views with templates for relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -54,29 +54,35 @@ def register(request):
 
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
-from .models import UserProfile
 
 
-def role_required(role):
-    def check(user):
-        return hasattr(user, "userprofile") and user.userprofile.role == role
-
-    return user_passes_test(check)
+# Check functions
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
 
 
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+# Views
 @login_required
-@role_required("Admin")
+@user_passes_test(is_admin)
 def admin_view(request):
     return render(request, "relationship_app/admin_view.html")
 
 
 @login_required
-@role_required("Librarian")
+@user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, "relationship_app/librarian_view.html")
 
 
 @login_required
-@role_required("Member")
+@user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
