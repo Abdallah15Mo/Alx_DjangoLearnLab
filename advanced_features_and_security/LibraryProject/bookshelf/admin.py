b6677dee9ee5227479d6ed64daa2b141d7696945
Abdallah mostafa <3bdallahgoat@gmail.com>
2025-07-27 11:54:56 +0300

    Add custom user model with date_of_birth and profile_photo

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,3 +8,26 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")  # columns in the list view
     list_filter = ("author", "publication_year")  # filters on the right
     search_fields = ("title", "author")  # search bar
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff")
+
+    fieldsets = UserAdmin.fieldsets + (
+        (_("Additional Info"), {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (_("Additional Info"), {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+
+# ✅ This is the line being checked:
+admin.site.register(CustomUser, CustomUserAdmin)
