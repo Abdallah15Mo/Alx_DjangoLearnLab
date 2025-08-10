14c3d2f57ebadcec318aa5d62150e9728006ee51
Abdallah mostafa <3bdallahgoat@gmail.com>
2025-08-10 12:56:09 +0300

    we are going to make this work

--- advanced-api-project/urls.py
+++ advanced-api-project/urls.py
@@ -5,3 +5,12 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),  # ✅ this line connects your app's URLs
 ]
+
+
+from django.contrib import admin
+from django.urls import path, include  # ✅ include is required
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),  # ✅ contains "api.urls"
+]
