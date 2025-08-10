9487c645f626e7f8774e03d1a8ae0c3940784ef6
Abdallah mostafa <3bdallahgoat@gmail.com>
2025-08-10 12:52:25 +0300

    we are going to make this work

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,8 @@
-from rest_framework.permissions import ( IsAuthenticatedOrReadOnly,IsAuthenticated,IsAdminUser,)
-from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from django.shortcuts import render
 
 
 class BookListView(generics.ListAPIView):
@@ -34,4 +34,3 @@ class BookUpdateView(generics.UpdateAPIView):
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAdminUser]
