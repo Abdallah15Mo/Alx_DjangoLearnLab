876fced59132636d9de6607546813685ee816804
Abdallah mostafa <3bdallahgoat@gmail.com>
2025-08-24 12:17:23 +0300

    we are going to make this work

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -73,3 +73,35 @@ def user_feed(request):
     posts = Post.objects.filter(author__in=followed_users).order_by("-created_at")
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
+
+
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
+from .serializers import UserSerializer
+
+
+class FollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        if user_to_follow == request.user:
+            return Response(
+                {"detail": "You can't follow yourself."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        request.user.following.add(user_to_follow)
+        return Response({"detail": f"You are now following {user_to_follow.username}."})
+
+
+class UnfollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({"detail": f"You have unfollowed {user_to_unfollow.username}."})
